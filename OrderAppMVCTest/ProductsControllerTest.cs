04dbae5aab0438140d086ff234accaffd815c201
Patrik2 <platoriansk@gmail.com>
2016-06-03 16:32:42 +0200

    Update Test By Fluent Assertion

--- OrderAppMVCTest/ProductsControllerTest.cs
+++ OrderAppMVCTest/ProductsControllerTest.cs
@@ -11,6 +11,7 @@ using System.Web.Mvc;
 using Test2.Controllers;
 using Test2.Interfaces;
 using Test2.Models;
+using FluentAssertions;
 
 namespace OrderAppMVCTest
 {
@@ -26,7 +27,7 @@ namespace OrderAppMVCTest
         {
             mockOrderServices = new Mock<IOrderServices>();
             mockCartServices = new Mock<ICartServices>();
-            controller = new ProductsController(mockOrderServices.Object,mockCartServices.Object);
+            controller = new ProductsController(mockOrderServices.Object, mockCartServices.Object);
         }
         [TestMethod()]
         public void TestIndexAction()
@@ -38,29 +39,29 @@ namespace OrderAppMVCTest
             var result = controller.Index();
 
             mockCartServices.Verify(x => x.GetListCartItemViewModel(), Times.Once());
-            Assert.IsNotNull(result);
-            Assert.IsNotNull(result.Model);
-            Assert.AreEqual(expectedCartItem, ((List<CartItemViewModel>)result.Model).SingleOrDefault());
-            Assert.AreEqual("Index", result.ViewName);
+
+            result.Should().NotBeNull();
+            result.Model.Should().NotBeNull();
+            ((List<CartItemViewModel>)result.Model).Should().ContainSingle().Which.ShouldBeEquivalentTo(expectedCartItem);
+            result.ViewName.ShouldBeEquivalentTo("Index");
         }
         [TestMethod()]
         public void TestCartAction()
         {
-            var expectedCartItem = new CartItem {ProductItemId=1, Quantity =2 };
-            var expectedCartItems = new List<CartItem> {expectedCartItem };
+            var expectedCartItem = new CartItem { ProductItemId = 1, Quantity = 2 };
+            var expectedCartItems = new List<CartItem> { expectedCartItem };
             var expectedCartItemViewModel = new CartItemViewModel { ProductItemId = 1, ItemPrice = 2, Quantity = 4 };
             var expectedListCartItemsViewModel = new List<CartItemViewModel> { expectedCartItemViewModel };
-            var expectedProductsAndCartItemViewModel = new ProductsAndCartItemViewModel { CartItems = expectedCartItems, CartItemViewModels= expectedListCartItemsViewModel };
-            //var expectedViewModel = new List<ProductsAndCartItemViewModel> {};
+            var expectedProductsAndCartItemViewModel = new ProductsAndCartItemViewModel { CartItems = expectedCartItems, CartItemViewModels = expectedListCartItemsViewModel };
             mockCartServices.Setup(x => x.GetModelForCartView()).Returns(expectedProductsAndCartItemViewModel);
 
             var result = controller.Cart();
 
             mockCartServices.Verify(x => x.GetModelForCartView(), Times.Once());
-            Assert.IsNotNull(result);
-            Assert.IsNotNull(result.Model);
-            Assert.AreEqual(expectedProductsAndCartItemViewModel, (ProductsAndCartItemViewModel)result.Model);
-            Assert.AreEqual("Cart", result.ViewName);
+
+            result.Should().NotBeNull();
+            result.Model.ShouldBeEquivalentTo(expectedProductsAndCartItemViewModel);
+            result.ViewName.ShouldBeEquivalentTo("Cart");
         }
         [TestMethod()]
         public void TestCartPostAction()
@@ -69,37 +70,36 @@ namespace OrderAppMVCTest
 
             var result = controller.Cart(cartItems);
 
-            mockCartServices.Verify(x => x.SetCart(cartItems) , Times.Once());
-            Assert.IsNotNull(result);
-            Assert.AreEqual("Cart", result.ViewName);
+            mockCartServices.Verify(x => x.SetCart(cartItems), Times.Once());
+            result.Should().NotBeNull();
+            result.ViewName.ShouldBeEquivalentTo("Cart");
         }
         [TestMethod()]
         public void TestCreateOrderAction()
         {
             var expectedCartItem = new CartItem { ProductItemId = 8, Quantity = 5 };
-            var cartItems = new List<CartItem> { expectedCartItem};
+            var cartItems = new List<CartItem> { expectedCartItem };
             var order = new Order { OrderItems = new List<OrderItem> { new OrderItem { Quantity = expectedCartItem.Quantity, OrderItemID = expectedCartItem.ProductItemId } } };
             mockOrderServices.Setup(x => x.CreateOrder(cartItems)).Returns(order);
 
             var result = controller.CreateOrder(cartItems);
 
             var resultOrder = result.Model as Order;
-            Assert.IsNotNull(result);
-            Assert.IsNotNull(resultOrder);
-            Assert.IsNotNull(resultOrder.OrderItems);
-            Assert.AreEqual(expectedCartItem.Quantity, resultOrder.OrderItems.Single().Quantity);
-            Assert.AreEqual(expectedCartItem.ProductItemId, resultOrder.OrderItems.Single().OrderItemID);
-            Assert.AreEqual("CreateOrder", result.ViewName);
+
+            result.Should().NotBeNull();
+            resultOrder.Should().NotBeNull();
+            resultOrder.OrderItems.Should().NotBeNullOrEmpty().And.ContainSingle().Which.ShouldBeEquivalentTo(expectedCartItem, opt => opt.ExcludingMissingMembers());
+            result.ViewName.Should().BeEquivalentTo("CreateOrder");
         }
         [TestMethod()]
         public void TestRemoveAction()
         {
             var result = controller.Remove(2);
 
-            Assert.IsNotNull(result);
-            Assert.IsFalse(result.Permanent);
             mockCartServices.Verify(x => x.RemoveItemFromCurrentCart(It.IsAny<int>()), Times.Once);
-            Assert.AreEqual("Cart", result.RouteValues["action"]);
+
+            result.Should().NotBeNull();
+            result.RouteValues["action"].ShouldBeEquivalentTo("Cart");
         }
     }
 }
